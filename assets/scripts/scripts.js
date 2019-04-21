521fbe97919ea461df7ac54131342d60e9cf84de
ievavold <ievavold@wisc.edu>
2019-04-21 12:09:49 -0500

    briefly change text on copy-to-clipboard button after click

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,1 +1,12 @@
-new ClipboardJS('.docs-uses-clipboard');
+(function(ClipboardJS) {
+  const buttonTextDelay = 1500; // milliseconds
+  const clipboard = new ClipboardJS('.docs-uses-clipboard');
+  clipboard.on('success', event => {
+    const elem = event.trigger;
+    const wasText = elem.innerText;
+    elem.innerText = 'Copied';
+    setTimeout(() => {
+      elem.innerText = wasText;
+    }, buttonTextDelay);
+  });
+})(ClipboardJS);
