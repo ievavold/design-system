5cf90cd106fa66d5f7bb6863d95f5fdca4792876
ievavold <ievavold@wisc.edu>
2019-04-23 14:40:26 -0500

    Updates

--- scripts/scripts.min.f4272f5f2e72a281945357d7e6857a5a4b132a7f80a9adcff4a88af16ba6abfc.js
+++
@@ -1,1 +0,0 @@
-(function(ClipboardJS){const buttonTextDelay=1500;const clipboard=new ClipboardJS('.docs-uses-clipboard');clipboard.on('success',event=>{const elem=event.trigger;const wasText=elem.innerText;elem.innerText='Copied';setTimeout(()=>{elem.innerText=wasText;},buttonTextDelay);});})(ClipboardJS);
---
+++ site.js
@@ -0,0 +1,20 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('clipboard')) :
+  typeof define === 'function' && define.amd ? define(['clipboard'], factory) :
+  (global = global || self, factory(global.ClipboardJS));
+}(this, function (ClipboardJS) { 'use strict';
+
+  var buttonTextDelay = 1500; // milliseconds
+  var cb = new ClipboardJS('.docs-uses-clipboard');
+  cb.on('success', function (event) {
+      var elem = event.trigger;
+      if (elem instanceof HTMLElement) {
+          var wasText_1 = elem.innerText;
+          elem.innerText = 'Copied';
+          setTimeout(function () {
+              elem.innerText = wasText_1;
+          }, buttonTextDelay);
+      }
+  });
+
+}));
