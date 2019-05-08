4ba67a112ec4bf19f93f057d998d68aa0a09afcf
ievavold <ievavold@wisc.edu>
2019-05-08 12:32:29 -0500

    Updates

--- site.js
+++ site.js
@@ -135,7 +135,97 @@
     context.addEventListener('keydown', handleKeydown, false);
   }
 
+  const GLOBALS = {
+    container: null
+  };
+
+  function mountContainer() {
+    if (document.querySelector('#uw-toast-container')) {
+      return (GLOBALS.container = document.querySelector('#uw-toast-container'));
+    }
+
+    const container = document.createElement('div');
+    container.id = 'uw-toast-container';
+    document.body.appendChild(container);
+    GLOBALS.container = container;
+    return container;
+  }
+
+  function unmountContainer() {
+    if (GLOBALS.container) {
+      GLOBALS.container.remove();
+      GLOBALS.container = null;
+    }
+  }
+
+  class Toast {
+    constructor(options = {}) {
+      this.state = options.state || 'info';
+      this.duration = options.duration || 5000;
+      this.message = options.message || '';
+      this.element = createToastElement(this.state, this.message);
+    }
+  }
+
+  function createToastElement(state = 'info', message = '') {
+    const toastRoot = document.createElement('div');
+    toastRoot.classList.add('uw-toast');
+    toastRoot.classList.add(`uw-toast--${state}`);
+
+    const toastCard = document.createElement('div');
+    toastCard.classList.add('uw-toast__card');
+    toastRoot.appendChild(toastCard);
+
+    const toastIcon = document.createElement('div');
+    toastIcon.classList.add('uw-toast__icon');
+    toastCard.appendChild(toastIcon);
+
+    const toastBody = document.createElement('p');
+    toastBody.classList.add('uw-toast__body');
+    toastBody.innerText = `${message}`;
+    toastCard.appendChild(toastBody);
+
+    return toastRoot;
+  }
+
+  function showToast(toast) {
+    if (GLOBALS.container === null) {
+      mountContainer();
+    }
+
+    GLOBALS.container.appendChild(toast.element);
+
+    setTimeout(() => {
+      hideToastWithAnimation(toast);
+    }, toast.duration);
+  }
+
+  function hideToastWithAnimation(toast) {
+    toast.element.classList.add('uw-toast--removing');
+    setTimeout(() => {
+      hideToast(toast);
+    }, 100);
+  }
+
+  function hideToast(toast) {
+    toast.element.remove();
+
+    if (GLOBALS.container && GLOBALS.container.childNodes.length === 0) {
+      unmountContainer();
+    }
+  }
+
+  function init$1() {
+    return {
+      toast: options => showToast(new Toast(options))
+    };
+  }
+
   init();
+
+  window.UW = {
+    ...init$1()
+  };
 
   const buttonTextDelay = 1500; // milliseconds
   const cb = new ClipboardJS('.docs-uses-clipboard');
